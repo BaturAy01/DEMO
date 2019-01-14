a2e64cd9cf6cf7eb3f3d4adfd452d6f9411b726f
Batur Ay <baturunmaili@gmail.com>
2019-01-15 00:14:54 +0300

    Add Project

---
+++ VertigoJuniorDevDemo/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public enum PlayerTeam
+{
+    None,
+    BlueTeam,
+
+    RedTeam
+}
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private List<SpawnPoint> _sharedSpawnPoints = new List<SpawnPoint>();
+    System.Random _random = new System.Random();
+	float _closestDistance;
+    [Tooltip("This will be used to calculate the second filter where algorithm looks for closest friends, if the friends are away from this value, they will be ignored")]
+    [SerializeField] private float _maxDistanceToClosestFriend = 30;
+    [Tooltip("This will be used to calculate the first filter where algorithm looks for enemies that are far away from this value. Only enemies which are away from this value will be calculated.")]
+    [SerializeField] private float _minDistanceToClosestEnemy = 10;
+    [Tooltip("This value is to prevent friendly player spawning on top of eachothers. If a player is within the range of this value to a spawn point, that spawn point will be ignored")]
+    [SerializeField] private float _minMemberDistance = 2;
+
+
+    public DummyPlayer PlayerToBeSpawned;
+    public DummyPlayer[] DummyPlayers;
+
+    private void Awake()
+    {
+		_sharedSpawnPoints.AddRange(FindObjectsOfType<SpawnPoint>());
+
+		DummyPlayers = FindObjectsOfType<DummyPlayer>();
+        //print("Buraya system çalışınca giriyor");
+    }
+
+    #region SPAWN ALGORITHM
+    public SpawnPoint GetSharedSpawnPoint(PlayerTeam team)
+    {
+        List<SpawnPoint> spawnPoints = new List<SpawnPoint>(_sharedSpawnPoints.Count);
+        CalculateDistancesForSpawnPoints(team);
+        GetSpawnPointsByDistanceSpawning(/*Please add appropriate parameters here*/);
+        if (spawnPoints.Count <= 0)
+        {
+            //print("Buraya giriyor");
+            GetSpawnPointsBySquadSpawning(team, ref spawnPoints);
+        }
+        SpawnPoint spawnPoint = spawnPoints.Count <= 1 ? spawnPoints[0] : spawnPoints[_random.Next(0, (int)((float)spawnPoints.Count * .5f))];
+        spawnPoint.StartTimer();
+        //print("Buraya giriyor");
+        return spawnPoint;
+        
+    }
+
+    private void GetSpawnPointsByDistanceSpawning(/*Please add appropriate parameters here*/)
+    {
+		//Please apply your algorithm here
+    }
+
+    private void GetSpawnPointsBySquadSpawning(PlayerTeam team, ref List<SpawnPoint> suitableSpawnPoints)
+    {
+        if (suitableSpawnPoints == null)
+        {
+            suitableSpawnPoints = new List<SpawnPoint>();
+            //print("Buraya girmiyor");
+        }
+        //print("Buraya giriyor");
+        suitableSpawnPoints.Clear();
+        _sharedSpawnPoints.Sort(delegate (SpawnPoint a, SpawnPoint b)
+        {
+            if (a.DistanceToClosestFriend == b.DistanceToClosestFriend)
+            {
+                //print("Buraya giriyor");
+                // 0 ile 1 i değiştirmek birşeyi değiştirmiyor.
+                return 0;
+            }
+            if (a.DistanceToClosestFriend > b.DistanceToClosestFriend)
+            {
+                //print("Buraya girmiyor");
+                return 1;
+            }
+            //print("Buraya girmiyor");
+            return -1;
+        });
+        for (int i = 0; i < _sharedSpawnPoints.Count && _sharedSpawnPoints[i].DistanceToClosestFriend <= _maxDistanceToClosestFriend; i++)
+        {
+            if (!(_sharedSpawnPoints[i].DistanceToClosestFriend <= _minMemberDistance) && !(_sharedSpawnPoints[i].DistanceToClosestEnemy <= _minMemberDistance) && _sharedSpawnPoints[i].SpawnTimer <= 0)
+            {
+                suitableSpawnPoints.Add(_sharedSpawnPoints[i]);
+                //print("Buraya girmiyor");
+            }
+        }
+        if (suitableSpawnPoints.Count <= 0)
+        {
+            suitableSpawnPoints.Add(_sharedSpawnPoints[0]);
+            //print("Buraya giriyor");
+        }
+
+    }
+
+    private void CalculateDistancesForSpawnPoints(PlayerTeam playerTeam)
+    {
+        for (int i = 0; i < _sharedSpawnPoints.Count; i++)
+        {
+            _sharedSpawnPoints[i].DistanceToClosestFriend = GetDistanceToClosestMember(_sharedSpawnPoints[i].PointTransform.position, playerTeam);
+            _sharedSpawnPoints[i].DistanceToClosestEnemy = GetDistanceToClosestMember(_sharedSpawnPoints[i].PointTransform.position, playerTeam == PlayerTeam.BlueTeam ? PlayerTeam.RedTeam : playerTeam == PlayerTeam.RedTeam ? PlayerTeam.BlueTeam : PlayerTeam.None);
+            //print("Buraya Giriyor");
+        }
+    }
+
+    private float GetDistanceToClosestMember(Vector3 position, PlayerTeam playerTeam)
+    {
+        foreach (var player in DummyPlayers)
+        {
+            if (!player.Disabled && player.PlayerTeamValue != PlayerTeam.None && player.PlayerTeamValue == playerTeam && !player.IsDead())
+            {
+                float playerDistanceToSpawnPoint = Vector3.Distance(position, player.Transform.position);
+                if (playerDistanceToSpawnPoint < _closestDistance)
+                {
+                    _closestDistance = playerDistanceToSpawnPoint;
+                    
+                    //print("Buraya girmiyor");
+
+                }
+            }
+        }
+        return _closestDistance;
+    }
+
+    #endregion
+	/// <summary>
+	/// Test için paylaşımlı spawn noktalarından en uygun olanını seçer.
+	/// Test oyuncusunun pozisyonunu seçilen spawn noktasına atar.
+	/// </summary>
+    public void TestGetSpawnPoint()
+    {
+    	SpawnPoint spawnPoint = GetSharedSpawnPoint(PlayerToBeSpawned.PlayerTeamValue);
+    	PlayerToBeSpawned.Transform.position = spawnPoint.PointTransform.position;
+        //print("Test spawnı buradan yapıyor");
+    }
+
+}
